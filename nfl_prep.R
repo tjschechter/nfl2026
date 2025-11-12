0816984f7ec6d3db4cdca4af7c82006fb5d71761
Joey <tjschechter@yahoo.com>
2025-11-12 11:57:25 -0800

    package loading, set up, management

--- nfl_prep.R
+++ nfl_prep.R
@@ -1,4 +1,20 @@
 ## script for data pulling, preparation and initial exploration
 
-## load requisite libraries
+## script for setting up Kaggle API for scraping data
+
+## install dev tools from CRAN
+install.packages(c("devtools"))
+
+
+## install dev version of kaggler from CRAN
+## install kaggler package from github
+devtools::install_github("koderkow/kaggler")
+
+## install necessary packages
+install.packages("pacman")
+
+## package manager
+library(pacman)
+## use package manager (pacman) to install, load all packages needed for this script
+p_load("tidyverse")
 
